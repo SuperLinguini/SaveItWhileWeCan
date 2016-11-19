ba05cab38dc99c754cb6ce1d04ea690e9c300d55
Shane Austrie <yoyo.rom.dev@gmail.com>
2016-11-19 17:20:51 -0500

    Fifth push by Shane

--- app/src/main/java/com/example/briandesouza/saveitwhilewecan/LoginActivity.java
+++ app/src/main/java/com/example/briandesouza/saveitwhilewecan/LoginActivity.java
@@ -11,6 +11,7 @@ import com.facebook.login.widget.LoginButton;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -368,6 +369,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
