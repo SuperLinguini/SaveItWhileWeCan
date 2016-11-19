72cb9c9dd1d6a3361f51b01c2fdd173f0c0b0732
Shane Austrie <yoyo.rom.dev@gmail.com>
2016-11-19 16:04:15 -0500

    First push by Shane

--- app/src/main/java/com/example/briandesouza/saveitwhilewecan/MainActivity.java
+++ app/src/main/java/com/example/briandesouza/saveitwhilewecan/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.briandesouza.saveitwhilewecan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button yesterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
