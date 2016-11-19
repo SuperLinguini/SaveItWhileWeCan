cac3ac6ca9f145d3c12ff791a995bb8040bd3e7f
Milind Lingineni <milind.lingineni@gmail.com>
2016-11-19 18:35:29 -0500

    Finalized tabs

--- app/src/main/java/com/example/briandesouza/saveitwhilewecan/MainActivity.java
+++ app/src/main/java/com/example/briandesouza/saveitwhilewecan/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.briandesouza.saveitwhilewecan;
 
 import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +25,25 @@ public class MainActivity extends AppCompatActivity {
         monthButton = (TextView) findViewById(R.id.monthScore);
         tipBoxButton = (TextView) findViewById(R.id.tipBox);
 
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_home) {
+
+                } else if (tabId == R.id.tab_todo) {
+                    Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                } else if (tabId == R.id.tab_daily) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                } else if (tabId == R.id.tab_monthly) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                }
+            }
+        });
 
     }
 
