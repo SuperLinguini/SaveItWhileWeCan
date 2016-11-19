1b73dce95f38c064fe07f3d7f4919a0101736961
Shane Austrie <yoyo.rom.dev@gmail.com>
2016-11-19 16:25:51 -0500

    Second push by Shane

--- app/src/main/java/com/example/briandesouza/saveitwhilewecan/MainActivity.java
+++ app/src/main/java/com/example/briandesouza/saveitwhilewecan/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.briandesouza.saveitwhilewecan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button yesterButton;
+    private TextView overallButton, toButton, monthButton, tipBoxButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        overallButton = (TextView) findViewById(R.id.overallScore);
+        toButton = (TextView) findViewById(R.id.todayScore);
+        monthButton = (TextView) findViewById(R.id.monthScore);
+        tipBoxButton = (TextView) findViewById(R.id.tipBox);
+
+
     }
 }
