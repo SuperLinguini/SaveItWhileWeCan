d084723a19458b1d3b68b1e31741797e2b21ab59
Shane Austrie <yoyo.rom.dev@gmail.com>
2016-11-20 08:12:20 -0500

    Another push by Shane

--- app/src/main/java/com/example/briandesouza/saveitwhilewecan/MapActivity.java
+++ app/src/main/java/com/example/briandesouza/saveitwhilewecan/MapActivity.java
@@ -1,5 +1,8 @@
 package com.example.briandesouza.saveitwhilewecan;
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
@@ -9,12 +12,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.esri.android.map.GraphicsLayer;
 import com.esri.android.map.MapView;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
+import com.esri.android.map.ags.ArcGISFeatureLayer;
+import com.esri.core.geometry.Envelope;
+import com.esri.core.geometry.Point;
+import com.esri.core.map.Feature;
+import com.esri.core.map.FeatureResult;
+import com.esri.core.map.Graphic;
+import com.esri.core.symbol.SimpleMarkerSymbol;
+import com.esri.core.tasks.query.QueryParameters;
+import com.esri.core.tasks.query.QueryTask;
 
 public class MapActivity extends AppCompatActivity {
-    MapView mMapView;
+    private MapView mMapView;
+    private ArcGISFeatureLayer mFeatureLayer;
+    private GraphicsLayer mGraphicsLayer;
+    private String mFeatureServiceURL;
+    ProgressDialog progress;
 
     // Called when the activity is first created.
     @Override
@@ -24,6 +41,16 @@ public class MapActivity extends AppCompatActivity {
 
         // After the content of this Activity is set, the map can be accessed programmatically from the layout.
         mMapView = (MapView) findViewById(R.id.map);
+
+        // Get the feature service URL from values->strings.xml
+        mFeatureServiceURL = this.getResources().getString(R.string.data);
+        // Add Feature layer to the MapView
+        mFeatureLayer = new ArcGISFeatureLayer(mFeatureServiceURL, ArcGISFeatureLayer.MODE.ONDEMAND);
+        mMapView.addLayer(mFeatureLayer);
+        // Add Graphics layer to the MapView
+        mGraphicsLayer = new GraphicsLayer();
+        mMapView.addLayer(mGraphicsLayer);
+        new QueryFeatureLayer().execute();
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar2);
         bottomBar.setDefaultTab(R.id.tab_map);
@@ -68,5 +95,75 @@ public class MapActivity extends AppCompatActivity {
             mMapView.unpause();
         }
     }
+
+    /**
+     * Run the query task on the feature layer and put the result on the map.
+     */
+    private class QueryFeatureLayer extends AsyncTask<Void, Void, FeatureResult> {
+
+        // default constructor
+        public QueryFeatureLayer() {
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(MapActivity.this, "", "Please wait....query task is executing");
+        }
+
+        @Override
+        protected FeatureResult doInBackground(Void... v) {
+
+            // Define a new query and set parameters
+            QueryParameters mParams = new QueryParameters();
+            mParams.setReturnGeometry(true);
+
+            // Define the new instance of QueryTask
+            QueryTask queryTask = new QueryTask(mFeatureServiceURL);
+            FeatureResult results;
+
+            try {
+                // run the querytask
+                results = queryTask.execute(mParams);
+                return results;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(FeatureResult results) {
+
+            // Remove the result from previously run query task
+            mGraphicsLayer.removeAll();
+
+            // Define a new marker symbol for the result graphics
+            SimpleMarkerSymbol sms = new SimpleMarkerSymbol(Color.BLUE, 10, SimpleMarkerSymbol.STYLE.CIRCLE);
+
+            // Envelope to focus on the map extent on the results
+            Envelope extent = new Envelope();
+
+            // iterate through results
+            for (Object element : results) {
+                // if object is feature cast to feature
+                if (element instanceof Feature) {
+                    Feature feature = (Feature) element;
+                    // convert feature to graphic
+                    Graphic graphic = new Graphic(feature.getGeometry(), sms, feature.getAttributes());
+                    // merge extent with point
+                    extent.merge((Point)graphic.getGeometry());
+                    // add it to the layer
+                    mGraphicsLayer.addGraphic(graphic);
+                }
+            }
+
+            // Set the map extent to the envelope containing the result graphics
+            mMapView.setExtent(extent, 100);
+            // Disable the progress dialog
+            progress.dismiss();
+
+        }
+    }
+
 
 }
