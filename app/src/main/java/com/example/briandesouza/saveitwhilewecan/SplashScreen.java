6c0ae5b86151d2072ca2ffb48a80ee6b344c3775
pablogarces16 <pablogarcesee@gmail.com>
2016-11-20 11:51:44 -0500

    Splash Screen Added

---
+++ app/src/main/java/com/example/briandesouza/saveitwhilewecan/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.briandesouza.saveitwhilewecan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
